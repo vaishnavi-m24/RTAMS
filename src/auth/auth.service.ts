825c6553f7dff162b8b3d7bd00f126c7e6b4da64
vaishnavi-m24 <vaishnavi.moharekar@tekditechnologies.com>
2024-08-19 14:53:14 +0530

    authentication changes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,41 @@
+// // import { Injectable } from '@nestjs/common';
+
+// // @Injectable()
+// // export class AuthService {}
+
 // import { Injectable } from '@nestjs/common';
+// import { JwtService } from '@nestjs/jwt';
+// import { UserService } from '../users/users.service';
 
 // @Injectable()
-// export class AuthService {}
+// export class AuthService {
+//   constructor(
+//     private userService: UserService,
+//     private jwtService: JwtService,
+//   ) {}
+
+//   async validateUser(username: string, pass: string): Promise<any> {
+//     const user = await this.userService.findOne(username);
+//     if (user && user.password === pass) { 
+//       const { password, ...result } = user;
+//       return result;
+//     }
+//     return null;
+//   }
+
+//   async login(user: any) {
+//     const payload = { username: user.username, sub: user.userId };
+//     return {
+//       access_token: this.jwtService.sign(payload),
+//     };
+//   }
+// }
+
 
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -14,9 +44,9 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.password === pass) { 
+  async validateUser(mobileNumber: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByMobileNumber(mobileNumber);
+    if (user && await bcrypt.compare(pass, user.password)) {
       const { password, ...result } = user;
       return result;
     }
@@ -24,7 +54,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { mobileNumber: user.mobileNumber, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
